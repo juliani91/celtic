276380bcf0ae71679c113c808b429dab7f2879b1
Julian <Julian@Loeracbi.com>
2016-05-26 15:02:44 -0500

    Adding files to gh-pages

---
+++ js/function.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$("#hamb").click(function(){
+		$("#myNav").css("height", "100%");
+	});
+
+	$("#cross").click(function(){
+		$("#myNav").css("height", "0%");
+	});
+
+
+
+});
